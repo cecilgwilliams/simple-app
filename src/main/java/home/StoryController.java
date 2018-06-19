4e80c0337f8bbe828f7a42690f3a52cc79e3f75e
Cecil G. Williams <cecil.g.williams@gmail.com>
2018-06-19 13:38:17 -0500

    reverted back to controller that uses map in memory to test that cert plugin is not expected database

--- src/main/java/home/StoryController.java
+++ src/main/java/home/StoryController.java
@@ -10,20 +10,17 @@ import java.util.*;
 @RequestMapping("/stories")
 public class StoryController {
 
-    private StoryRepository storyRepo;
-
-    public StoryController(StoryRepository storyRepo) {
-        this.storyRepo = storyRepo;
-    }
+    private long counter = 1;
+    private Map<Long, Story> stories = new HashMap<>();
 
     @GetMapping
     public ResponseEntity<List<Story>> list() {
-        return new ResponseEntity<>(storyRepo.list(), HttpStatus.OK);
+        return new ResponseEntity<>(new ArrayList<>(stories.values()), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Story> read(@PathVariable Long id) {
-        Story story = storyRepo.find(id);
+        Story story = stories.get(id);
         if (story != null) {
             return new ResponseEntity<>(story, HttpStatus.OK);
         } else {
@@ -31,25 +28,40 @@ public class StoryController {
         }
     }
 
-    @PutMapping("{id}")
-    public ResponseEntity<Story> update(@PathVariable Long id, @RequestBody Story story) {
-        Story updatedStory = storyRepo.update(id, story);
-        if (updatedStory != null) {
-            return new ResponseEntity<>(updatedStory, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
     @PostMapping
     public ResponseEntity<Story> createStory(@RequestBody Story story){
-        Story createdStory = storyRepo.create(story);
-        return new ResponseEntity<>(createdStory, HttpStatus.CREATED);
+        story.setId(counter);
+        stories.put(counter, story);
+        counter++;
+        return new ResponseEntity<>(story, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Story> updateStory(@PathVariable Long id, @RequestBody Story story){
+        Story localStory = stories.get(id);
+        localStory.setProjectId(story.getProjectId());
+        localStory.setName(story.getName());
+        localStory.setInfo(story.getInfo());
+        stories.put(id, localStory);
+        return new ResponseEntity<>(localStory, HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Story> delete(@PathVariable Long id) {
-        storyRepo.delete(id);
+        stories.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/reset")
+    public ResponseEntity<Story> reset(){
+        stories.clear();
+        counter = 1;
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/clear")
+    public ResponseEntity<Story> clear(){
+        stories.clear();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
